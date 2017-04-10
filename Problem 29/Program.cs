edaced0db171d63150329879ccf54b7b68dc335a
Ronald Johnson <ronjohn83@hotmail.com>
2017-04-10 16:22:11 -0400

    Initial Commit

---
+++ Problem 29/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DistinctPowers());
+            Console.Read();
+        }
+
+        private static double DistinctPowers()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int counter = 0;
+            double power = 0;
+            List<double> list = new List<double>();
+
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    power = Math.Pow(i, j);
+                    list.Add(power);
+                }
+            }
+
+            list.Sort();
+            List<double> distinct = list.Distinct().ToList();
+            counter = distinct.Count;
+            stopwatch.Stop();
+
+            return counter;
+        }
+    }
+}
